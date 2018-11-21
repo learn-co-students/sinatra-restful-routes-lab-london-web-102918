ef2ca83b009c08bf3471602cf96a5bc98f22aa9c
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-21 19:54:23 +0000

    Created table, migrations, class, basic structure of pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,23 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/' do
+    erb :index
+  end
+
+  get'/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :recipes
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    @recipe.update
+    erb :recipes
+  end
+
 end
